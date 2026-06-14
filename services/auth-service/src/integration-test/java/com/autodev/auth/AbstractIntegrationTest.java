588fc83d8311effa67b41615e3fa7b88dce166c6
Aleksey Shvariov <alxsshv@gmail.com>
2026-06-14 15:48:50 +0000

    feature/AUTH-002-update-migrations

---
+++ services/auth-service/src/integration-test/java/com/autodev/auth/AbstractIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.autodev.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@SpringBootTest
+@Testcontainers
+public class AbstractIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:15-alpine")
+            .withInitScript("init-schemas.sql");
+
+    @DynamicPropertySource
+    public static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    public void loadContext() {
+
+    }
+}
