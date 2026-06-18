8fa95d5e077156f36dabc1e44e19bc9630011751
Aleksey Shvariov <alxsshv@gmail.com>
2026-06-18 13:18:47 +0000

    feature/AUTH-005-update-user-repository

--- services/auth-service/src/main/java/com/autodev/auth/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/autodev/auth/repository/UserRepository.java
@@ -6,11 +6,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+/** Репозиторий для пользователей {@link User} */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /** Метод поиска пользователей по KeycloakUserId.
+     * @param keycloakUserId  - уникальный идентификатор пользователя в keycloak*/
     Optional<User> findByKeycloakUserId(String keycloakUserId);
 
+
+    /** Метод поиска пользователей по email.
+     * @param email - адрес электронной посты пользователя */
     Optional<User> findByEmail(String email);
 
 }
