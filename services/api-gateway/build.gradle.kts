1dc8e228b315253863a1509d0a4cec77ffc61ca0
Aleksey Shvariov <alxsshv@gmail.com>
2026-07-11 21:23:23 +0300

    Выполнена задача № 003-3-api-gateway-cors-filter.md. Добавлен CORS фильтр, добавлены тесты

--- services/api-gateway/build.gradle.kts
+++ services/api-gateway/build.gradle.kts
@@ -2,6 +2,7 @@ import java.time.Duration
 
 plugins {
     java
+    id("jacoco")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
 }
@@ -104,4 +105,27 @@ tasks.register<Test>("integrationTest") {
 
 tasks.check {
     dependsOn("integrationTest")
+}
+
+tasks.register<JacocoReport>("jacocoAggregatedReport") {
+    group = "verification"
+    description = "Generates aggregated JaCoCo coverage report for all subprojects"
+
+    dependsOn(tasks.test, tasks.named("integrationTest"))
+
+    // Собираем execution data из всех подпроектов
+    executionData(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
+
+    // Указываем исходники и классы для анализа (можно перечислить все подпроекты)
+    subprojects.forEach { subproject ->
+        subproject.plugins.withType<JavaPlugin> {
+            sourceSets(subproject.sourceSets.main.get())
+        }
+    }
+
+    reports {
+        xml.required.set(true)   // для GitLab нужен XML
+        html.required.set(true)
+        csv.required.set(false)
+    }
 }
