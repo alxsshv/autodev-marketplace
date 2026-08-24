215e634997c85e8e267d642d566c57750d3e7d6c
Aleksey Shvariov <alxsshv@gmail.com>
2026-08-24 14:50:16 +0300

    feat(listing-service): create listing service

---
+++ services/catalog-service/src/main/java/com/autodev/catalogservice/entity/CategoryEntity.java
@@ -0,0 +1,12 @@
+package com.autodev.catalogservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity extends AbstractBaseEntity {
+
+
+
+}
